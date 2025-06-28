4aac9b57ae03975d37a185a8ecf0f83922986898
Samu <samy.munoz.s@gmail.com>
2025-06-28 18:28:23 -0500

    detalle de apadrinamiento

--- src/app/administrador/components/detalle-solicitud/detalle-solicitud.component.ts
+++ src/app/administrador/components/detalle-solicitud/detalle-solicitud.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Inject, inject, Input, OnInit, Output } from '@angular/core';
 import { SolicitudAdopcion } from '../../../shared/model/solicitud-adopcion';
 import { SolicitudAdopcionService } from '../../../shared/services/solicitud-adopcion/solicitud-adopcion.service';
 import { Router } from '@angular/router';
+import { SolicitudApadrinamiento } from '../../../shared/model/solicitud-apadrinamiento';
+import { SolicitudApadrinamientoService } from '../../../shared/services/solicitud-apadrinamiento/solicitud-apadrinamiento.service';
 
 @Component({
   selector: 'app-detalle-solicitud',
@@ -16,6 +18,7 @@ export class DetalleSolicitudComponent implements OnInit {
   detalle: { key: string; value: string }[] = [];
 
   solicitudAdopcionService: SolicitudAdopcionService = inject(SolicitudAdopcionService);
+  solcitudApadrinamientoservice:  SolicitudApadrinamientoService = inject(SolicitudApadrinamientoService);
   constructor(
     private route: Router
   ) {}
@@ -26,6 +29,10 @@ export class DetalleSolicitudComponent implements OnInit {
       let objeto = null;
       switch(detalle.tipo) {
         case "solicitud-adopcion":
+          objeto = detalle.objeto as SolicitudApadrinamiento;
+          this.solicitud = { tipo: detalle.tipo, id: objeto.idSolicitud! }
+          break;
+        case "solicitud-apadrinamiento":
           objeto = detalle.objeto as SolicitudAdopcion;
           this.solicitud = { tipo: detalle.tipo, id: objeto.idSolicitud! }
           break;
@@ -42,6 +49,11 @@ export class DetalleSolicitudComponent implements OnInit {
       case "solicitud-adopcion":
         this.solicitudAdopcionService.eliminarSolicitudAdopcion(this.solicitud.id);
         this.route.navigate(["/admin/solicitudes-adopcion"]);
+        break;
+      case "solicitud-apadrinamiento":
+        this.solcitudApadrinamientoservice.eliminarSolicitudApadrinamiento(this.solicitud.id);
+        this.route.navigate(["/admin/solicitud-apadrinamiento"]);
+        break;
     } 
 
   }
--- src/app/administrador/pages/solicitud-apadrinamiento/solicitud-apadrinamiento.component.ts
+++ src/app/administrador/pages/solicitud-apadrinamiento/solicitud-apadrinamiento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from '@angular/core';
 import { SolicitudApadrinamientoService } from '../../../shared/services/solicitud-apadrinamiento/solicitud-apadrinamiento.service';
 import { SolicitudApadrinamiento } from '../../../shared/model/solicitud-apadrinamiento';
 import { from, groupBy, map, mergeMap, toArray, filter } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-solicitud-apadrinamiento',
@@ -13,6 +14,12 @@ export class SolicitudApadrinamientoComponent implements OnInit {
   mostrarLoading: boolean = false;
   solicitudesApadrinamiento: { fecha: string; tipo: string; items: SolicitudApadrinamiento[] }[] = [];
   apadrinamientoService: SolicitudApadrinamientoService = inject(SolicitudApadrinamientoService);
+
+  constructor(
+    private router:Router
+    ){
+
+  }
 
   ngOnInit(): void {
     this.obtenerSolicitudesApadrinamiento();
@@ -49,8 +56,10 @@ export class SolicitudApadrinamientoComponent implements OnInit {
       });
   }
 
-  redireccionarADetalle() {
-    console.log("Detalle de solicitud de apadrinamiento");
+  redireccionarADetalle(item:any) {
+  const detalle = { tipo: "solicitud-apadrinamiento", objeto: item as SolicitudApadrinamiento };
+  localStorage.setItem("detalle", JSON.stringify(detalle));
+  this.router.navigate(["/admin/detalles"]);
   }
 }
 
